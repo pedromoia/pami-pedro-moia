4d53b606ca23e36e928d8da067613ad970029fbd
Pedro Moia <pmmoia74ads@gmail.com>
2026-03-19 22:24:02 -0300

    Aula sobre variaveis no JavaScript

---
+++ aula01/Basico.js
@@ -0,0 +1,19 @@
+// Javascript é uma linguagem fracamente tipada
+
+var VariavelVar = "Olá"
+let VariavelLet = 'Tudo bem?'
+const VariavelConst = 13
+
+console.log("Olá Mundo!")
+console.log(VariavelVar)
+console.log(VariavelVar, VariavelLet)
+console.log(VariavelVar + VariavelLet)
+console.log(VariavelConst + VariavelConst)
+
+VariavelLet = 4
+console.log(VariavelLet)
+
+// VariavelConst = 6
+// A regra é que a constante não pode
+//  ser retribuida
+console.log(VariavelConst)
