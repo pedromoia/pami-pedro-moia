b6746640a3a3ce732432fbbf376c795a6abd78c5
Pedro Moia <pmmoia74ads@gmail.com>
2026-03-26 22:16:00 -0300

    Aula sobre funções e números

---
+++ aula02/funcoes.js
@@ -0,0 +1,27 @@
+// Pascal é um padrão de escrita que separa as 
+//palavras onde toda sentenca inicia e maiusculo.
+
+function SomaNumeros () {
+    let valor_i = 6
+    let valor_ii = 5
+    let resultado = 0
+    
+    
+    return resultado  = valor_i + valor_ii
+}
+
+console.log(SomaNumeros())
+function SomaNumerosII (valor_i, valor_ii) {
+    console.log(valor_i + valor_ii)
+
+}
+
+SomaNumerosII(9, 15)
+SomaNumerosII(3, 4)
+SomaNumerosII(90, 50)
+
+const SOMA_NUMEROS_III = (valor_i, valor_ii) => {
+return valor_i , valor_ii
+}
+
+console.log(SOMA_NUMEROS_III(81, 98))
---
+++ aula02/numeros.js
@@ -0,0 +1,40 @@
+// Snake case é caracterizado por utilizar o underline/underscore (_)
+// para separar suas palavras . Pode ser combinado ent6re letras
+// maiusculas (screaming) ou minusculas.
+let primeiro_valor = 0
+let segundo_valor = 0
+
+primeiro_valor = 4
+segundo_valor = 5
+
+// - , / e *
+
+console.log(
+    `${primeiro_valor} + ${segundo_valor} = `,
+    primeiro_valor + segundo_valor
+)
+
+console.log(
+    `${primeiro_valor} - ${segundo_valor} = `,
+    primeiro_valor - segundo_valor
+)
+
+console.log(
+    `${primeiro_valor} / ${segundo_valor} = `,
+    primeiro_valor / segundo_valor
+)
+console.log(
+    `${primeiro_valor} * ${segundo_valor} = `,
+    primeiro_valor * segundo_valor
+)
+
+
+console.log(typeof(primeiro_valor))
+
+let variavel = 'oie'
+console.log(typeof(variavel))
+
+
+
+
+
